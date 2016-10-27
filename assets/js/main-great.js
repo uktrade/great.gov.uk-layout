260564a8ae312aa1d1d699c81690509f8b2ab96b
Onkar Chiroma <onkar.chiroma@MCL096.local>
2016-10-27 15:53:39 +0100

    add loading, extract js into separate file

---
+++ assets/js/main-great.js
@@ -0,0 +1,47 @@
+equalheight(".great-equal-hights-container");
+setupLocaleSelector();
+blurLinks();
+removeloading();
+
+
+function blurLinks() {
+    $('a').click(function(event) {
+        $(this).blur();
+    });
+}
+
+function setupLocaleSelector() {
+    $('.great-locale-selctor__toggle').click(function(event) {
+        event.preventDefault();
+        slideToggle('.great-locale-selctor');
+    });
+
+    $('.great-locale-selctor').hide();
+}
+
+function removeloading() {
+    $('.dit-loading').fadeOut(300);
+}
+
+function slideToggle(item) {
+    // $(item).slideToggle("easeInOutSine");
+    $(item).slideToggle('2000', 'easeInOutSine', function () {
+        // Animation complete.
+    });
+};
+
+function scrollingBG() {
+    // Y axis scroll speed
+    var velocity = 0.15;
+
+    function update(){ 
+    var pos = $(window).scrollTop(); 
+    var $element = $(this);
+    // subtract some from the height b/c of the padding
+    var height = $element.height()+50;
+        $('body').css('backgroundPosition', '0 ' + Math.round(-(height - pos) * velocity) + 'px'); 
+    };
+
+    $(window).bind('scroll', update);
+    
+};
